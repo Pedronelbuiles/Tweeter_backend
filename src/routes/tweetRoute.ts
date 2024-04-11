64c57bf6b6a9a2d4c3f2dd992e84718f57b0d097
Pedro Nel Builes Moreno <pedronel.builes@gmail.com>
2024-04-11 12:38:37 -0500

    finished activity of crud from tweets

--- src/routes/tweetRoute.ts
+++ src/routes/tweetRoute.ts
@@ -1,25 +1,54 @@
+import { PrismaClient } from '@prisma/client';
 import { Router } from 'express'
 
 const router = Router()
+const prisma = new PrismaClient()
 
-
-
-router.post("/", (req, res) => {
-    res.status(501).json({ error: "Not implemented" })
+router.get("/", async(req, res) => {
+    const allTweets = await prisma.tweet.findMany()
+    res.json(allTweets)
 });
 
-router.get("/", (req, res) => {
-    res.status(501).json({ error: "Not implemented" })
-});
-
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     const { id } = req.params
-    res.status(501).json({ error: `Not implemented ${id}` })
+    const tweet = await prisma.tweet.findUnique({ where: { id: id } })
+    res.json(tweet)
 });
 
-router.delete("/:id", (req, res) => {
-    const { id } = req.params
-    res.status(501).json({ error: `Not implemented ${id}` })
+router.post("/", async (req, res) => {
+    const { content, image, userId } = req.body
+    try {
+        const result = await prisma.tweet.create({
+            data: {
+                content,
+                image,
+                userId
+            }
+        })
+        res.json(result)
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
 });
+
+router.put("/:id", async (req, res) => {
+    const { id } = req.params
+    const { content, image } = req.body
+    try {
+        const result = await prisma.tweet.update({
+            where: {id: id},
+            data: { content , image, updatedAt: new Date() }
+    })
+    res.json(result)
+    } catch (error) {
+        res.status(400).json({ error: `Falied to update the tweet` })
+    }
+});
+
+router.delete("/:id",  async(req, res) => {
+    const { id } = req.params
+    await prisma.tweet.delete({ where: {id: id} })
+    res.sendStatus(200)
+})
 
 export default router
